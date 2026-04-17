52d89474c6ac684b06c031f0e3ea88814347016c
Rinad adel <studentrenad@gmail.com>
2026-04-17 20:53:59 +0300

    add view to create deck

--- decks/views.py
+++ decks/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Deck
+from .forms import DeckForm
 
-# Create your views here.
+@login_required
+def all_deck(request):
+    all_deck = Deck.objects.all()
+@login_required
+def create_deck(request):
+    # create_deck = Deck.objects.create()
+    form = DeckForm(request.POST)
+    if form.is_valid():
+        new_deck = form.save(commit=False)
+    
+        new_deck.user = request.user
+
+        new_deck.save()
+    else:
+        form = DeckForm()
+
+    context = {'form': form}
+    return render(request, 'decks/templates/deck_namager.html', context)
