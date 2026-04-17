3a283ca298589227e84f63fee3ead6d7c4e0bc0b
Rinad adel <studentrenad@gmail.com>
2026-04-17 20:56:48 +0300

    add model card

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from decks.models import Deck
 
-# Create your models here.
+class Card(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    front_q = models.TextField()
+    back_a = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
