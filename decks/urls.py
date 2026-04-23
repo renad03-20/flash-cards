4f0119bf655e7bd189b4ab3fab1ec7060a7b57ab
Rinad adel <studentrenad@gmail.com>
2026-04-23 23:56:29 +0300

    fix urls

--- decks/urls.py
+++ decks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create_deck', views.create_deck, name='create_deck'),
+    path('', views.home_page, name='Decks'),
+    path('create_deck/', views.create_deck, name='create_deck'),
     path('delete_deck/<int:deck_id>', views.delete_deck, name='delete_deck')
 ]
