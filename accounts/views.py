fd1d7872fdf600436288770e944b308f9950a2a5
Rinad adel <studentrenad@gmail.com>
2026-04-27 23:53:17 +0300

    add index, login, reigster views

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,47 @@ from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
+from django.conf import settings
+
+def index(request):
+    return render(request, 'index.html', {'title':'index'})
 
 def register(request):
-    ...
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+
+            htmly = get_template('user/Email.html')
+            d = { 'username': username }
+            subject, from_email, to = 'welcome', settings.EMAIL_HOST_USER, email
+            html_content = htmly.render(d)
+            msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+            messages.success(request, f'your account has been created, you are now able to log in')
+
+            return redirect('login')
+        
+    else:
+        form = UserRegisterForm()
+        return render(request, 'register.html', {'form': form, 'title': 'register here'})
+    
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            form = login(request, user)
+            messages.success(request, f' welcome {username} !!')
+            return redirect('index')
+        else:
+            messages.info(request, f'account does not exist please sign up')
+    form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form, 'title': 'login'})
