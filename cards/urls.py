221aa7fa12baef7f0e504c3a2817c3f2cd2609bb
Rinad adel <studentrenad@gmail.com>
2026-05-02 23:56:26 +0300

    add urls for views

--- cards/urls.py
+++ cards/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    ...
+    path('<int:deck_id>/options', views.options, name='options'),
+    path('<int:deck_id>/add_card/', views.add_card, name='add_card'),
+    path('<int:deck_id>/study/', views.study, name='study'),
 ]
