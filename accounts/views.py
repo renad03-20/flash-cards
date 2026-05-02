56a36bffb7e38842a910d2efc30d5aef059aeca6
Rinad adel <studentrenad@gmail.com>
2026-05-02 23:54:08 +0300

    delete index page

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login   
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserRegisterForm
-from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
-from django.template import Context
 from django.conf import settings
-
-def index(request):
-    return render(request, 'index.html', {'title':'index'})
 
 def register(request):
     if request.method == 'POST':
@@ -21,34 +16,38 @@ def register(request):
             username = form.cleaned_data.get('username')
             email = form.cleaned_data.get('email')
 
-            htmly = get_template('Email.html')
-            d = { 'username': username }
-            subject, from_email, to = 'welcome', settings.EMAIL_HOST_USER, email
-            html_content = htmly.render(d)
-            msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
-            msg.attach_alternative(html_content, "text/html")
-            msg.send()
+            try:
+                htmly = get_template('Email.html')
+                html_content = htmly.render({'username': username})
+                subject = 'Welcome to FlashCards!'
+                from_email = settings.EMAIL_HOST_USER
+                msg = EmailMultiAlternatives(subject, html_content, from_email, [email])
+                msg.attach_alternative(html_content, "text/html")
+                msg.send()
+            except Exception:
+                pass  
 
-            messages.success(request, f'your account has been created, you are now able to log in')
-
+            messages.success(request, f'Account created! You can now log in.')
             return redirect('login')
-        
+
     else:
         form = UserRegisterForm()
-        return render(request, 'register.html', {'form': form, 'title': 'register here'})
-    
+
+    return render(request, 'register.html', {'form': form, 'title': 'Sign Up'})
+
+
 def login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username = username, password = password)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            form = login(request, user)
-            messages.success(request, f' welcome {username} !!')
-            return redirect('index')
+            auth_login(request, user)
+            messages.success(request, f'Welcome back, {username}!')
+            return redirect('Decks')
         else:
-            messages.info(request, f'account does not exist please sign up')
-    form = AuthenticationForm()
+            messages.error(request, 'Invalid username or password. Please try again.')
 
-    return render(request, 'login.html', {'form': form, 'title': 'login'})
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form, 'title': 'Log In'})
