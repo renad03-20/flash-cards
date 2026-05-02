f75cfc47072fbded77b63dded96b9fd35d1ddc13
Rinad adel <studentrenad@gmail.com>
2026-05-02 23:55:59 +0300

    add options, add cards, and study views

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Card
+from decks.models import Deck
+from .forms import cardForm
 
-# Create your views here.
+@login_required
+def options(request, deck_id):
+    deck = get_object_or_404(Deck, id=deck_id)
+    return render(request, 'options.html', {'deck': deck})
+
+@login_required
+def add_card(request, deck_id):
+    deck = get_object_or_404(Deck, id=deck_id)
+    return render(request, 'addCard.html', {'deck': deck})
+
+@login_required
+def study(request, deck_id):
+    deck = get_object_or_404(Deck, id=deck_id)
+    return render(request, 'study.html', {'deck': deck})
