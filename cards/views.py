169e0986d1d28949a56e4d7fa2dd029e9a7a37aa
Rinad adel <studentrenad@gmail.com>
2026-05-04 23:39:25 +0300

    complete add card view

--- cards/views.py
+++ cards/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Card
 from decks.models import Deck
-from .forms import cardForm
+from .forms import CardForm
 
 @login_required
 def options(request, deck_id):
@@ -11,8 +12,20 @@ def options(request, deck_id):
 
 @login_required
 def add_card(request, deck_id):
-    deck = get_object_or_404(Deck, id=deck_id)
-    return render(request, 'addCard.html', {'deck': deck})
+    deck = get_object_or_404(Deck, id=deck_id, user=request.user)
+    
+    if request.method == 'POST':
+        form = CardForm(request.POST)
+        if form.is_valid():
+            card = form.save(commit=False)
+            card.deck = deck
+            card.save()
+            messages.success(request, f'cards are added now you can study them!')           
+            return redirect('study', deck_id=deck.id)
+    else:
+        form = CardForm()
+        
+    return render(request, 'options.html', {'form': form, 'deck': deck})
 
 @login_required
 def study(request, deck_id):
