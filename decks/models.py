f3dab81220022f12d7889f0483afc4e6291f384b
Rinad adel <studentrenad@gmail.com>
2026-05-09 23:15:20 +0300

    add readable string representation

--- decks/models.py
+++ decks/models.py
@@ -5,3 +5,6 @@ class Deck(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
