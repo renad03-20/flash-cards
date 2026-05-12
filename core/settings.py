fa00c489bd9935f8d113d461b56032bc88a99eed
Rinad adel <studentrenad@gmail.com>
2026-05-12 23:57:59 +0300

    add login url

--- core/settings.py
+++ core/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+LOGIN_URL = 'login'
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
